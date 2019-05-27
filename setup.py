361a2f8ffb58653f7540257801d4070e2a7560a3
chenguang du <duchengaung@licaigc.com>
2019-05-27 10:01:13 +0800

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="pyexchange",
+    version="1.0",
+    author="strengthening",
+    author_email="strengthen2010@gmail.com",
+    packages=["pyexchange", "pyexchange.okex"],
+)
